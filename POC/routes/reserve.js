c89fc30e25078796d6c34658f550f854f6fbf25f
Durga Abayakumar <42677554+durgaabhay@users.noreply.github.com>
2018-10-09 20:02:11 -0400

    updated for phone reservation

--- POC/routes/reserve.js
+++ POC/routes/reserve.js
@@ -67,18 +67,66 @@ router.post('/inbound', (req,res,next) => {
     let body = req.body.Body;
     let from = req.body.from;
     let to = req.body.to;
+    let reserveTime = Date.parse(body);
+    let seats = body;
+    console.log('reserveTime', reserveTime);
     Customer.findOne({phoneNumber : req.body.from}).exec()
         .then( foundCustomer => {
             console.log('Does the customer exists ?', foundCustomer.length);
             if(foundCustomer.length == 1) {
-                if (body == 'Reserve') {
-
-                } else {
+                if (body === 'Reserve') {
                     client.messages.create({
                         to :`${from}`,
                         from :`${to}`,
-                        body : 'Do you wish to make a reservation? Please send message Reserve to the same number.'
+                        body : 'How many people are you making a reservation for?'
+                    })
+                } else if(!isNaN(body)){//checking if body entered value is a number
+                    Customer.updateOne({phoneNumber:req.body.from}, {$set: {noOfSeats: body}}).exec();
+                    client.messages.create({
+                        to :`${from}`,
+                        from :`${to}`,
+                        body : 'Time of reservation? Specify in this format: 2018-10-08 13:35'
+                    })
+                }
+                else if (reserveTime instanceof Date){
+                    console.log('yes, user entered reserveTime correctly' , reserveTime);
+                    //add new tables only for phone reservations
+                    Reservation.findOne({tableType:'RESERVATION'}).exec()
+                        .then( foundTable => {
+                            if(foundTable.length >= 1){
+                                Reservation.updateOne({_id:foundTable[0]._id} , {$set : {customerName: foundCustomer.customerName, status:'RESERVED', bookingDate: reserveTime}})
+                            }else{
+                                Customer.updateOne({phoneNumber:from}, {$set : {tableStatus : 'INQUEUE'}}).exec()
+                                    .then(result => {
+                                        client.messages.create({
+                                            to : `${from}`,
+                                            from : `${to}`,
+                                            body : 'Welcome"' + req.body.userName + '"to Awesome Food. Thank you for your patience. Once a table is ready you will make a reservation'
+                                        });
+                                        res.status(200).json({
+                                            message : 'Putting user in queue for reservation',
+                                            customerDetails : {result}
+                                        })
+                                    }).catch(err => {
+                                        res.status(500).json({
+                                            message : 'Error adding reservation for user',
+                                            error : err
+                                        })
+                                })
+                            }
+                        }).catch(err => {
+                            res.status(500).json({
+                               message : 'Error finding table for reservation',
+                                error : err
+                            });
                     });
+                }
+                    else {
+                        client.messages.create({
+                                to :`${from}`,
+                                from :`${to}`,
+                                body : 'Do you wish to make a reservation? Please send message Reserve to the same number.'
+                        });
                 }
             }else{
                 //create new customer first
